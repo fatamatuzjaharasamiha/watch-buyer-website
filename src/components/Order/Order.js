05e36e5f14020e5a8083d7db79640c26cb32d141
fatamatuzjaharasamiha <samirajahara843@gmail.com>
2022-04-03 23:47:02 +0600

    react first milestone

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Order = (props) => {
     console.log(props.watch)
+    const { name, picture } = props.watch
     return (
         <div className='fw-bold'>
-            Name : {props.watch.name}
+            {/* Name : {props.watch.name} */}
+            
+            <img src={picture} className='w-25' alt="" />
+            name:{name}
         </div>
     );
 };
